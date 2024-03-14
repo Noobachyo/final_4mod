d00358a9c1054355d5a0052d6563a43e35175a83
Noobachyo <serezha_dubinin_2004@mail.ru>
2024-03-14 14:24:59 +0700

    init

---
+++ supermovie/src/routes/movie/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+  let movies = [
+    {title: 'За Палыча!', description: 'Отчественное кино о народном духе и братстве'},
+    {title: 'Чебурашка', description: 'Отчественное кино о необычном существе, которое очень любит апельсины'},
+    {title: 'Русы против ящеров', description: 'Отчественное кино о легендарной битве с зелеными негодяями'},
+
+  ];
+
+  function toggleDescription (movie)
+  {
+    movie.showDescription = !movie.showDescription;
+  }
+</script>
+
+<style>
+  .movie {
+    margin: 10 px;
+    display: inline-block;
+    position: relative;
+  }
+
+  .movie:hover .description {
+    display: block;
+  }
+
+  .description {
+    display: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    background-color: lightgray;
+    padding: 10px;
+    border: 1px solid darkgray;
+  }
+</style>
+
+{#each movies as movie}
+  <div class="movie">
+  <h3>{movie.title}</h3>
+  <p on:mouseenter={() => toggleDescription(movie)} on:mouseleave={() => toggleDescription(movie)}>
+    {#if movie.showDescription}
+     <div class="description">{movie.description}</div>
+    {/if}
+  </div>
+{/each}
